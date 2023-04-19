14337c5a3530771f121a87ccd1a4c9d2f93db4d2
luis1gonzalez <luis1gonzalez@hotmail.com>
2023-04-19 23:46:48 +0200

    primeros pasos

--- src/shared/Header.jsx
+++ src/shared/Header.jsx
@@ -3,7 +3,7 @@ import logo from '../assets/logos/logo-menu.png'
 
 export default function Header() {
   return (
-    <div className='h-[200px] py-1 flex justify-center'>
+    <div className='h-[20px] py-1 flex justify-center'>
         <img className='h-[100%]' src={logo} alt="logo de la estancia" />
     </div>
   )
