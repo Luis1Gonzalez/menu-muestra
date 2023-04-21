69eb8587be5b95ac21e6765ec7195762045b0b90
luis1gonzalez <luis1gonzalez@hotmail.com>
2023-04-22 00:23:51 +0200

    agregando onClick

--- src/data/sections.js
+++ src/data/sections.js
@@ -3,12 +3,14 @@ import cuatroQuesos from '../assets/entrantes/2.png'
 import tequeños from '../assets/entrantes/3.png'
 import cheezBacon from '../assets/entrantes/4.png'
 import nachos from '../assets/entrantes/5.png'
+import alitas from '../assets/entrantes/6.png'
 
 const menuEntrantes = [
     {id:1, name:'Patatas Bravas', description:'Patatas fritas con salsa picante.', image: patatasBravas },
     {id:2, name:'Plato Cuatro Quesos', description:'Plato de combinado de cuatro quesos.',  image: cuatroQuesos },
     {id:3, name:'Tequeños', description:'PRación de tequeños.', image: tequeños },
     {id:4, name:'Patatas Cheez&Bacon', description:'Patatas fritas con con quespo fundido y bacon.', image: cheezBacon },
-    {id:5, name:'Nachos', description:'Nachos con carne picada y queso americano fundido.', image: nachos }
+    {id:5, name:'Nachos', description:'Nachos con carne picada y queso americano fundido.', image: nachos },
+    {id:6, name:'Alitas de Pollo', description:'Alitas de pollo en salsa BBQ.', image: alitas }
 ]
 export { menuEntrantes }
