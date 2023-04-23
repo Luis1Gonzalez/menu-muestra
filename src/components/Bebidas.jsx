57e44589a06a22bfe833652e8ab09dc72a33c495
luis1gonzalez <luis1gonzalez@hotmail.com>
2023-04-23 12:50:49 +0200

    haciendo el menu

--- src/components/Bebidas.jsx
+++ src/components/Bebidas.jsx
@@ -29,15 +29,18 @@ export default function Bebidas() {
             <div className='wrap-bebida' style={{ display: `${selectBebidas}` }}>
 
                 <ol className='bebidas-bebida-list'>
-                    <li>Vino de la Casa</li>
-                    <li>Vino de la Rioja</li>
-                    <li>Vino de Cariñena</li>
-                    <li>Cerveza</li>
-                    <li>Agua</li>
-                    <li>Refresco</li>
-                    <li>Limonada</li>
-                    <li>Ladron de Manzanas</li>
-                    <li>Sidra</li>
+                    <li>Vino de la Casa (botella) <span>10€</span></li>
+                    <li>Vino de la Casa (copa) <span>2.5€</span></li>
+                    <li>Vino de la Rioja (botella) <span>13€</span></li>
+                    <li>Vino de la Rioja (copa) <span>4€</span></li>
+                    <li>Vino de Cariñena (botella) <span>12€</span></li>
+                    <li>Vino de Cariñena (copa) <span>3.5€</span></li>
+                    <li>Cerveza <span>3€</span></li>
+                    <li>Agua <span>2€</span></li>
+                    <li>Refresco <span>2.5€</span></li>
+                    <li>Limonada <span>3€</span></li>
+                    <li>Ladron de Manzanas <span>3€</span></li>
+                    <li>Sidra <span>3.5€</span></li>
                 </ol>
 
 
--- src/components/LaCarta.jsx
+++ src/components/LaCarta.jsx
@@ -30,34 +30,34 @@ export default function LaCarta() {
 
                 <p className='laCarta-course-title'>PLatos</p>
                 <ol className='laCarta-course-list'>
-                    <li>Codillo de Cerdo al Horno</li>
-                    <li>Espagueti Carbonara</li>
-                    <li>Hamburguesa de Ternazco</li>
-                    <li>Merluza a la Riojana</li>
-                    <li>Pulpo a la Plancha</li>
-                    <li>Pechuga de Pollo al ajillo</li>
-                    <li>Risoto</li>
-                    <li>Ternasco de Aragon</li>
-                    <li>Conejo al Salmorejo</li>
+                    <li>Codillo de Cerdo al Horno <span>8€</span></li>
+                    <li>Espagueti Carbonara <span>7€</span></li>
+                    <li>Hamburguesa de Ternazco <span>8€</span></li>
+                    <li>Merluza a la Riojana <span>10€</span></li>
+                    <li>Pulpo a la Plancha <span>14€</span></li>
+                    <li>Pechuga de Pollo al ajillo <span>8€</span></li>
+                    <li>Risoto <span>9€</span></li>
+                    <li>Ternasco de Aragon <span>9€</span></li>
+                    <li>Conejo al Salmorejo <span>8€</span></li>
                 </ol>
 
                 <p className='laCarta-course-title'>Ensaladas</p>
                 <ol className='laCarta-course-list'>
-                    <li>Ensalada Campera</li>
-                    <li>Ensalada Capressa</li>
-                    <li>Ensalada Cesar</li>
-                    <li>Ensalada de Gambas con Salsa Fria de Merluza</li>
-                    <li>Ensalada Griega</li>
-                    <li>Ensalada Olivier</li>
+                    <li>Ensalada Campera <span>7€</span></li>
+                    <li>Ensalada Capressa <span>7€</span></li>
+                    <li>Ensalada Cesar <span>7€</span></li>
+                    <li>Ensalada de Gambas con Salsa<span>8€</span></li>
+                    <li>Ensalada Griega <span>7€</span></li>
+                    <li>Ensalada Olivier <span>8€</span></li>
                 </ol>
 
                 <p className='laCarta-course-title'>Raciones</p>
                 <ol className='laCarta-course-list'>
-                    <li>Pan</li>
-                    <li>Patatas fritas</li>
-                    <li>Patatas al Horno</li>
-                    <li>Tempura de Vegetales</li>
-                    <li>Arroz</li>
+                    <li>Pan <span>2€</span></li>
+                    <li>Patatas fritas <span>4€</span></li>
+                    <li>Patatas al Horno <span>4€</span></li>
+                    <li>Tempura de Vegetales <span>3€</span></li>
+                    <li>Arroz <span>4€</span></li>
                 </ol>
 
             </div>
